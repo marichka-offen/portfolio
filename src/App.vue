97819e50f13e8a940da1485bf4b5b620889ee4c5
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-02-27 11:35:22 -0500

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello, world</h1>
+    <TopNavbar />
+    <AppRouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import TopNavbar from '@/components/TopNavbar.vue'
+</script>
+
+<style scoped></style>
