d2441827df7f09034ec2ae1bdd76f306f53217cb
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-02-28 13:00:16 -0500

    Add router transitions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    <h1>Hello, world</h1>
-    <TopNavbar />
-    <AppRouterView />
+  <div class="app">
+    <NavBar class="app__nav-bar" />
+    <div class="app_container">
+      <router-view v-slot="{ Component }">
+        <Transition name="slide" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </router-view>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import TopNavbar from '@/components/TopNavbar.vue'
+  import NavBar from '@/components/NavBar.vue'
 </script>
 
-<style scoped></style>
+<style>
+  .app {
+    @apply h-screen
+    w-screen
+    relative;
+  }
+
+  .app_container {
+    @apply h-full
+    w-full
+    p-8;
+  }
+
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translate(-100%);
+  }
+
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: all 0.3s ease-out;
+  }
+</style>
