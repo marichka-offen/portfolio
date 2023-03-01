873356a4478945b99efe4bc9a6931bbf657fc0bf
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-02-28 20:07:25 -0500

    Create ContentBlock.vue

---
+++ src/components/ContentBlock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content-block">
+    <div class="content-block__header">
+      <slot name="header" class="content-block__header" />
+    </div>
+    <div class="content-block__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+  .content-block {
+    @apply flex
+        flex-col
+        gap-4;
+  }
+
+  .content-block__header {
+    @apply text-2xl
+    border-b-2
+    border-zinc-500;
+  }
+</style>
