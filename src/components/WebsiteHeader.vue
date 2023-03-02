0bc475293cd3048e29d9cac8ab3cfecc792ec9c2
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-02 15:36:19 -0500

    Remove unnecessary code from websiteHeader

--- src/components/WebsiteHeader.vue
+++ src/components/WebsiteHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="website-header">
-    <h1 class="website-header__title">Marichka</h1>
+    <div class="website-header__title">Marichka</div>
 
     <div>
       <button v-if="!isDarkMode">
@@ -21,27 +21,18 @@
 
 <style>
   .website-header {
-    @apply /* fixed */
-    /* top-0 */
-    w-full
+    @apply w-full
     flex
     justify-between
     items-center
-    p-8
-    /* shadow-md */
-    /* bg-slate-300 */
-    backdrop-blur-sm
-    bg-opacity-20
-    z-40;
+    p-8;
   }
 
   .website-header__title {
-    @apply text-7xl
-    text-slate-900;
+    @apply text-6xl
+    text-slate-900
+    relative;
     font-family: 'Yanone Kaffeesatz', sans-serif;
-    /* color: #f9f1cc;
-    text-shadow: -15px 5px 20px #ced0d3;
-    text-shadow: 5px 5px 0px #ffb650, 10px 10px 0px #ffd662, 15px 15px 0px #ff80bf,
-      20px 20px 0px #ef5097, 25px 25px 0px #6868ac, 30px 30px 0px #90b1e0; */
+    line-height: 0.6;
   }
 </style>
