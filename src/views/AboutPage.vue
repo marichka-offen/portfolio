06f9a4f1798b6ba275cd6caa7b370d1a98f1727d
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-01 23:06:56 -0500

    Update AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,46 +1,59 @@
-import ContentBlock from '@/components/ContentBlock.vue'
-
 <template>
-  <ContentBlock class="about-page">
-    <template #header> About </template>
-    <template #default>
-      <div class="about-page__bio">
-        <div class="about-page__bio-text">
-          Hello, I'm Marichka, a US-based front-end developer and programming enthusiast. As a
-          self-taught web developer, I've dedicated countless hours to learning and mastering HTML,
-          CSS, and JavaScript, and I've honed my skills through a combination of passion, personal
-          growth, and hard work.
-          <br /><br />
-          I take pride in creating beautiful, intuitive, and responsive user interfaces for websites
-          and applications. As a team player and aspiring mentor, I enjoy collaborating with others
-          and sharing my knowledge through walkthroughs and Q&A sessions.
-          <br /><br />
-          In addition to my development work, I am also a dedicated lifelong learner. I believe that
-          continuous education is essential for staying ahead of the curve in the ever-evolving
-          world of technology. If you have something to teach me, I'm all ears!
-        </div>
+  <div class="about-page">
+    <ContentBlock>
+      <template #header> About </template>
+      <template #default>
+        <div class="about-page__bio">
+          <div class="about-page__bio-text">
+            Hello, I'm Marichka, a US-based front-end developer and programming enthusiast. As a
+            self-taught web developer, I've dedicated countless hours to learning and mastering
+            HTML, CSS, and JavaScript, and I've honed my skills through a combination of passion,
+            personal growth, and hard work.
+            <br /><br />
+            I take pride in creating beautiful, intuitive, and responsive user interfaces for
+            websites and applications. As a team player and aspiring mentor, I enjoy collaborating
+            with others and sharing my knowledge through walkthroughs and Q&A sessions.
+            <br /><br />
+            In addition to my development work, I am also a dedicated lifelong learner. I believe
+            that continuous education is essential for staying ahead of the curve in the
+            ever-evolving world of technology. If you have something to teach me, I'm all ears!
+          </div>
 
-        <ImageWrapper
-          :image="me"
-          :gradient-colors="['#d6d0be', '#13181b']"
-          dimensions="360"
-          fit="cover"
-          radius
-        />
-      </div>
-    </template>
-  </ContentBlock>
+          <ImageWrapper2
+            :image="me"
+            :gradient-colors="['#d6d0be', '#13181b']"
+            dimensions="360"
+            fit="cover"
+            radius
+          />
+        </div>
+      </template>
+    </ContentBlock>
+    <ContentBlock>
+      <template #header> Skills </template>
+      <template #default>
+        <SkillsGallery />
+      </template>
+    </ContentBlock>
+  </div>
 </template>
 
 <script setup lang="ts">
   import ContentBlock from '@/components/ContentBlock.vue'
   import me from '@/assets/me.jpg'
-  import ImageWrapper from '@/components/ImageWrapper.vue'
+  import ImageWrapper2 from '@/components/ImageWrapper2.vue'
+  import SkillsGallery from '@/components/SkillsGallery.vue'
 </script>
 
 <style>
+  .about-page {
+    @apply flex
+    flex-col
+    gap-8
+  }
+
   .about-page__bio {
-      @apply flex
+    @apply flex
     gap-8
     flex-col
     lg:flex-row
