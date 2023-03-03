25b0b38387be6c73120621195784b75c1dc9a7d4
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-02 19:51:19 -0500

    Add contact page

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,5 +1,20 @@
 <template>
-  <main>Contact</main>
+  <ContentBlock>
+    <template #header> Contact </template>
+    <template #default>
+      Let's talk! Whether you have a question, need help, want to discuss job opportunities, or just
+      simply chat, feel free to reach out to me! I'm always happy to connect with new people and
+      hear about their experiences. Let's grab a virtual coffee and chat about anything and
+      everything. I promise to read each and every message and help in any way I can. Looking
+      forward to hearing from you!
+      <ExternalLink link="mailto: marichka.offen@gmail.com" text="marichka.offen@gmail.com" />
+      <ExternalLink link="https://github.com/marichka-offen" text="GitHub" />
+      <ExternalLink link="https://www.linkedin.com/in/marichka-offen/" text="LinkedIn" />
+    </template>
+  </ContentBlock>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ContentBlock from '@/components/ContentBlock.vue'
+  import ExternalLink from '@/components/ExternalLink.vue'
+</script>
