d25b56123e38ab5957b7aff16bd04c4733349904
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-17 01:35:36 -0400

    Update LanguageSkillsGallery.vue

--- src/components/LanguageSkillsGallery.vue
+++ src/components/LanguageSkillsGallery.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="language-skills-gallery">
-    <img :src="english" alt="Ukrainian" class="language-skills-gallery__skill" />
+    <div class="language-skills-gallery__skill">
+      <SkillLogoWrapper color="#dc2626">
+        <img :src="english" alt="English" class="language-skills-gallery__image" />
+      </SkillLogoWrapper>
+      <p class="text-[#dc2626]">English</p>
+    </div>
 
-    <img :src="ukrainian" alt="Ukrainian" class="language-skills-gallery__skill" />
+    <div class="language-skills-gallery__skill">
+      <SkillLogoWrapper color="#0284c7">
+        <img :src="ukrainian" alt="Ukrainian" class="language-skills-gallery__image" />
+      </SkillLogoWrapper>
+      <p class="text-[#0284c7]">Ukrainian</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import SkillLogoWrapper from '@/components/SkillLogoWrapper.vue'
+
   import english from '@/assets/icons/english.svg'
   import ukrainian from '@/assets/icons/ukrainian.svg'
 </script>
@@ -14,12 +26,26 @@
 <style>
   .language-skills-gallery {
     @apply flex
-        gap-8;
+    flex-wrap
+    justify-center
+    items-center
+    overflow-auto
+    gap-8
+    w-full;
   }
   .language-skills-gallery__skill {
-    @apply rounded-md
+    @apply flex
+    flex-col
+    gap-2
+    justify-center
+    items-center;
+  }
+
+  .language-skills-gallery__image {
+    @apply rounded-xl
     h-12
     w-16
+    p-1
     object-cover;
   }
 </style>
